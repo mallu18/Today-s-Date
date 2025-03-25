adf9eb13a4e76c303cc69e9ff0ff0b47cad05dc8
mallu18 <mallikarjunghasani18@gmail.com>
2025-03-25 16:23:18 +0000

    app.js

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const app = express()
 app.get('/', (req, res) => {
   const today = new Date()
 
-  response.send(
-    `${today.getDate()}-${today.getMonth() + 1}-${today.getFullYear()}`,
-  )
+  res.send(`${today.getDate()}-${today.getMonth() + 1}-${today.getFullYear()}`)
 })
 
 module.exports = app
